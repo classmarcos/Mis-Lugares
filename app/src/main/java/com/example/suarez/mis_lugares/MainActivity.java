a1e0ebd4a60dd3a438b4baad799be4396468b3ee
classmarcos <marcos.class07@gmail.com>
2015-05-07 15:52:40 -0400

    Agregando acciones a los botones acerdade y salir

--- app/src/main/java/com/example/suarez/mis_lugares/MainActivity.java
+++ app/src/main/java/com/example/suarez/mis_lugares/MainActivity.java
@@ -1,24 +1,33 @@
 package com.example.suarez.mis_lugares;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends ActionBarActivity {
+    Button salir;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_item, menu);
         return true;
     }
 
@@ -33,7 +42,20 @@ public class MainActivity extends ActionBarActivity {
         if (id == R.id.action_settings) {
             return true;
         }
+        if(id == R.id.acercaDe){
+            lanzarAcercaDe(null);
+            return true;
+        }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void lanzarAcercaDe(View view){
+        Intent i = new Intent(this, AcercaDe.class);
+        startActivity(i);
+    }
+
+    public void salir(View view){
+        finish();
     }
 }
