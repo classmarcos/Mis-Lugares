b1852a4bd91d8fefd624e38049e44b73d2cb9ccd
classmarcos <marcos.class07@gmail.com>
2015-05-08 17:16:12 -0400

    Agregando los cambios de vista y edicion en la aplicacion

--- app/src/main/java/com/example/suarez/mis_lugares/MainActivity.java
+++ app/src/main/java/com/example/suarez/mis_lugares/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.suarez.mis_lugares;
 
+import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
@@ -8,6 +9,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -47,8 +49,31 @@ public class MainActivity extends ActionBarActivity {
             lanzarAcercaDe(null);
             return true;
         }
+        if(id == R.id.menu_buscar) {
+            lanzarVistaLugar(null);
+            return true;
+        }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void lanzarVistaLugar(View view) {
+        final EditText entrada = new EditText(this);
+        entrada.setText("0");
+        new AlertDialog.Builder(this)
+                .setTitle("Selección de lugar")
+                .setMessage("indica su id:")
+                .setView(entrada)
+                .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        long id = Long.parseLong(entrada.getText().toString());
+                        Intent i = new Intent(MainActivity.this, VistaLugar.class);
+                        i.putExtra("id", id);
+                        startActivity(i);
+                    }
+                })
+                .setNegativeButton("Cancelar", null)
+                .show();
     }
 
     public void lanzarAcercaDe(View view){
---
+++ app/src/main/java/com/example/suarez/mis_lugares/VistaLugar.java
@@ -0,0 +1,142 @@
+package com.example.suarez.mis_lugares;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.util.Date;
+import android.view.View;
+
+/**
+ * Created by suarez on 08/05/2015.
+ */
+public class VistaLugar  extends ActionBarActivity {
+
+    private long id;
+    private Lugar lugar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.vista_lugar);
+        Bundle extras = getIntent().getExtras();
+        id = extras.getLong("id", -1);
+        lugar = Lugares.elemento((int) id);
+
+        actualizarVistas();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.vista_lugar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.accion_compartir:
+                return true;
+            case R.id.accion_llegar:
+                return true;
+            case R.id.accion_editar:
+                lanzarEdicion(null);
+                return true;
+            case R.id.accion_borrar:
+                confirmarBorrar(null);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void lanzarEdicion(View view) {
+        Intent i = new Intent(this, EdicionLugar.class);
+        i.putExtra("id", id);
+//        startActivity(i);
+        startActivityForResult(i, 1234);
+    }
+
+    public void confirmarBorrar(View view) {
+
+        new AlertDialog.Builder(this)
+                .setTitle("Borrado de lugar")
+                .setMessage("¿Estás seguro que quieres eliminar este lugar?")
+                .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        Lugares.borrar((int) id);
+                    }
+                })
+                .setNegativeButton("Cancelar", null)
+                .show();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 1234) {
+            actualizarVistas();
+            findViewById(R.id.scrollView1).invalidate();
+        }
+    }
+
+    public void actualizarVistas() {
+        TextView nombre = (TextView) findViewById(R.id.nombre);
+        nombre.setText(lugar.getNombre());
+        ImageView logo_tipo = (ImageView) findViewById(R.id.logo_tipo);
+        logo_tipo.setImageResource(lugar.getTipo().getRecurso());
+
+        TextView tipo = (TextView) findViewById(R.id.tipo);
+        tipo.setText(lugar.getTipo().getTexto());
+
+        if (lugar.getDireccion() == null) {
+            findViewById(R.id.direccion).setVisibility(View.GONE);
+        } else {
+            TextView direccion = (TextView) findViewById(R.id.direccion);
+            direccion.setText(lugar.getDireccion());
+        }
+        if (lugar.getTelefono() == 0) {
+            findViewById(R.id.telefono).setVisibility(View.GONE);
+        } else {
+            TextView telefono = (TextView) findViewById(R.id.telefono);
+            telefono.setText(Integer.toString(lugar.getTelefono()));
+        }
+
+        if (lugar.getUrl() == null) {
+            findViewById(R.id.url).setVisibility(View.GONE);
+        } else {
+            TextView url = (TextView) findViewById(R.id.url);
+            url.setText(lugar.getUrl());
+        }
+        if (lugar.getComentario() == null) {
+            findViewById(R.id.comentario).setVisibility(View.GONE);
+        } else {
+            TextView comentario = (TextView) findViewById(R.id.comentario);
+            comentario.setText(lugar.getComentario());
+        }
+        TextView fecha = (TextView) findViewById(R.id.fecha);
+        fecha.setText(DateFormat.getDateInstance().format(
+                new Date(lugar.getFecha())));
+        TextView hora = (TextView) findViewById(R.id.hora);
+        hora.setText(DateFormat.getTimeInstance().format(
+                new Date(lugar.getFecha())));
+        RatingBar valoracion = (RatingBar) findViewById(R.id.valoracion);
+        valoracion.setRating(lugar.getValoracion());
+        valoracion.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                lugar.setValoracion(rating);
+            }
+        });
+    }
+
+}
